bbaa9ebb7211302e9400fedf3e5666db4df57e80
DanielCX21 <daniel.almeida.castro@hotmail.com>
2025-02-26 10:34:57 -0300

    aprendendo

--- client_tcp_conexao_servidor.py
+++ client_tcp_conexao_servidor.py
@@ -1,15 +1,17 @@
 import socket
+import sys
 
 IP_adress = "0.tcp.sa.ngrok.io"
-port = 15669
+port = 15901
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.settimeout(1)
+client.settimeout(5)
 
 try:
     print(f"Tentando conexão a {IP_adress}:{port}...")
     client.connect((IP_adress, port))  # Conecta ao servidor remoto
     print("Conectado ao servidor")
+    print(client.recv(1024).decode())
 
     #exemplo de requisição
     #request = f"GET / HTTP/1.1\r\nHOST: {IP_adress}\r\n\r\n"
@@ -17,16 +19,27 @@ try:
     #e mandaria o código HTML para o navegador carregar o front-end
 
     j = 0
+    recebi = True
     while True:
-        try:
-            j += 1
-            mensagem = input(f"Digite uma mensagem {j}: ")
-            client.send(mensagem.encode())
+        if recebi == True:
+            recebi = False
+            try:
+                j += 1
+                mensagem = input(f"Digite uma mensagem {j}: ")
+                client.send(mensagem.encode())
+                if mensagem.strip() == "sair":
+                    break
+            except:
+                print("Mensagem nao enviada")
+                client.close()
+                sys.exit()
 
+        try:
             resposta = client.recv(1024).decode()
             print(f"Resposta do servidor {j}: ", resposta)
+            recebi = True
         except:
-            print("Nao conseguimos enviar mensagem ou receber")
+            pass
 
     client.close()
 
--- server_tcp.py
+++ server_tcp.py
@@ -3,7 +3,7 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 IP_local = "0.0.0.0"  # Aceita conexões de qualquer IP local
-porta_local = 8080     # Escolha uma porta livre
+porta_local = 8500    # Escolha uma porta livre
 #Após isso o Ngrok irá levar a porta 8080 ao mundo
 #Para isso o Ngrok precisa estar na mesma rede
 
@@ -18,18 +18,33 @@ try:
         client_socket, address = server.accept()
         print("Conexão recebida de:", address)
         #Vamos descobrir quem conectou em nós
-        while True:
-            data = client_socket.recv(1024).decode()
-            print("Mensagem recebida:", data)
-            if data == "sair\n":
-                break
+        try:
+            client_socket.send(b"Bem vindo!")
+        except:
+            pass
 
-            resposta = input("De uma resposta:")
-            client_socket.send(resposta.encode())
+        while True:
+            try:
+                data = client_socket.recv(1024).decode()
+                print("Mensagem recebida: ", data)
+            except:
+                pass
+            if data.strip() == "sair":
+                print("Cliente encerrou conexão")
+                client_socket.send(b"Adeus!")
+                client_socket.close()
+                break
+            try:
+                resposta = input("Digite a resposta: ")
+                client_socket.send(resposta.encode())
+            except:
+                pass
+            if resposta.strip() == "fechar":
+                client_socket.send(b"Adeus!")
+                server.close()
 
     server.close()
 
 except Exception as error:
     print("Erro:", error)
     server.close()
-
